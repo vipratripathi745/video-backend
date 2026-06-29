950b91468cb46ed913bee36ec6e45cb9a20bc556
vipratripathi745 <vipratripathi745@gmail.com>
2026-06-29 13:30:32 +0530

    Add Express configuration and utility classes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb",
+}));
+
 app.use(express.static("public"));
+
+app.use(cookieParser());
 
 export { app };
