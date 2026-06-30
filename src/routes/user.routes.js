688aaa9f98efb21875ed49538d148266efed27dd
vipratripathi745 <vipratripathi745@gmail.com>
2026-06-30 18:08:38 +0530

    Implement login API with JWT authentication

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
@@ -17,5 +17,7 @@ router.route("/register").post(
     ]),
     registerUser
 );
+
+router.route("/login").post(loginUser);
 
 export default router;
