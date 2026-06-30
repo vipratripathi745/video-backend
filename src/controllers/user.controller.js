6c52f09bb371e5d3c807bbe10a7c902bdc6a2a24
vipratripathi745 <vipratripathi745@gmail.com>
2026-06-30 22:16:43 +0530

    Complete JWT authentication module

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -231,9 +231,34 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user?._id);
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    user.password = newPassword;
+
+    await user.save({ validateBeforeSave: false });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "Password changed successfully"
+        )
+    );
+});
+
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -32,6 +33,11 @@ router.route("/logout").post(
 );
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(
+    verifyJWT,
+    changeCurrentPassword
+);
 
 router.route("/current-user").get(
     verifyJWT,
